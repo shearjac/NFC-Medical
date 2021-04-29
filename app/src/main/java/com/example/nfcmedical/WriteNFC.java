c4d19455c231a8445915860dcb76b05214a862d8
srpurr <57876095+srpurr@users.noreply.github.com>
2021-04-29 06:01:41 -0500

    Added "Done" button and redirect for WriteNFC

--- app/src/main/java/com/example/nfcmedical/WriteNFC.java
+++ app/src/main/java/com/example/nfcmedical/WriteNFC.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class WriteNFC extends AppCompatActivity {
@@ -23,5 +25,14 @@ public class WriteNFC extends AppCompatActivity {
         String nfcDataString = intent.getStringExtra("nfcDataString");
         TextView nfcWriteString = (TextView) findViewById(R.id.nfcWriteString);
         nfcWriteString.setText(nfcDataString);
+
+        Button doneButton = (Button) findViewById(R.id.nfcWriteDoneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PatientOptions.class); //redirect
+                startActivity(intent);
+            }
+        });
     }
 }
