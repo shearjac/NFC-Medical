5ef126b3d0830caec35b2c125519acc94d549a5a
LeuzziLuigi <40071913+LeuzziLuigi@users.noreply.github.com>
2021-04-29 15:09:44 -0500

    Vaccine date formatted
    humans can read it!

--- app/src/main/java/com/example/nfcmedical/DBClasses/Vaccine.java
+++ app/src/main/java/com/example/nfcmedical/DBClasses/Vaccine.java
@@ -43,7 +43,49 @@ public class Vaccine {
 
     @Override
     public String toString(){
-        return name + " " + date;
+        return name + " " + getDateFormat(date);
+    }
+
+    //GETS YYYY-MM-DD
+    //RETURNS Day Month Year
+    private String getDateFormat(String date){
+        String newDate;
+        String day;
+        String month;
+        String year;
+        year = String.valueOf(date.charAt(0)) + String.valueOf(date.charAt(1)) + String.valueOf(date.charAt(2)) + String.valueOf(date.charAt(1));
+        day = String.valueOf(date.charAt(8)) + String.valueOf(date.charAt(9));
+        int monthInt = Integer.parseInt(String.valueOf(date.charAt(5))+ String.valueOf(date.charAt(6)));
+        month = getMonthFormat(monthInt);
+        return day + " " + month + " " + year;
+    }
+
+    private String getMonthFormat(int month){
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+        return "JAN";
     }
 
 }
